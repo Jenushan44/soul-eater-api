0525d0841e48bf1e150bd2a4579c6f5ca9c26183
Jenushan <jenushan349@gmail.com>
2026-06-04 20:25:22 -0400

    feat: add hardcoded characters endpoint

--- app/main.py
+++ app/main.py
@@ -5,3 +5,13 @@ app = FastAPI()
 @app.get("/")
 def root(): 
   return {"message": "Soul Eater API is running"}
+
+@app.get("/characters")
+def get_characters():
+  return [
+    {"id": 1, "name": "Maka Albarn", "role": "Meister", "affiliation": "DWMA"}, 
+    {"id": 2, "name": "Soul Evans", "role": "Demon Weapon", "affiliation": "DWMA"},
+    {"id": 3, "name": "Black Star", "role": "Meister", "affiliation": "DWMA"},
+    {"id": 4, "name": "Tsubaki Nakatsukasa", "role": "Demon Weapon", "affiliation": "DWMA"},
+    {"id": 5, "name": "Death the Kid", "role": "Meister", "affiliation": "DWMA"},
+    ]
