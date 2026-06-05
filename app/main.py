69c2e1dc03052a68df17269fc6246fad11afd3d1
Jenushan <jenushan349@gmail.com>
2026-06-05 18:25:24 -0400

    feat: add character filtering by role

--- app/main.py
+++ app/main.py
@@ -7,14 +7,25 @@ def root():
   return {"message": "Soul Eater API is running"}
 
 @app.get("/characters")
-def get_characters():
-  return [
-    {"id": 1, "name": "Maka Albarn", "role": "Meister", "affiliation": "DWMA"}, 
-    {"id": 2, "name": "Soul Evans", "role": "Demon Weapon", "affiliation": "DWMA"},
-    {"id": 3, "name": "Black Star", "role": "Meister", "affiliation": "DWMA"},
-    {"id": 4, "name": "Tsubaki Nakatsukasa", "role": "Demon Weapon", "affiliation": "DWMA"},
-    {"id": 5, "name": "Death the Kid", "role": "Meister", "affiliation": "DWMA"},
-    ]
+def get_characters(role: str |  None):
+  filtered_role_characters = []
+
+  characters = [
+      {"id": 1, "name": "Maka Albarn", "role": "Meister", "affiliation": "DWMA"}, 
+      {"id": 2, "name": "Soul Evans", "role": "Demon Weapon", "affiliation": "DWMA"},
+      {"id": 3, "name": "Black Star", "role": "Meister", "affiliation": "DWMA"},
+      {"id": 4, "name": "Tsubaki Nakatsukasa", "role": "Demon Weapon", "affiliation": "DWMA"},
+      {"id": 5, "name": "Death the Kid", "role": "Meister", "affiliation": "DWMA"},
+      ]
+  
+  if role is None: 
+    return characters 
+  else: 
+    for character in characters: 
+      if character["role"] == role: 
+        filtered_role_characters.append(character)
+    
+    return filtered_role_characters
 
 @app.get("/characters/{character_id}")
 def get_character_by_id(character_id: int): 
