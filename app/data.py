bcf16485af5cb06a4bef59ff22190f35f7b9fb60
Jenushan <jenushan349@gmail.com>
2026-06-07 17:22:16 -0400

    refactor: move character data to separate file

---
+++ app/data.py
@@ -0,0 +1,7 @@
+characters = [
+  {"id": 1, "name": "Maka Albarn", "role": "Meister", "affiliation": "DWMA", "description": "A dedicated meister who partners with Soul Evans and works hard to become stronger."}, 
+  {"id": 2, "name": "Soul Evans", "role": "Demon Weapon", "affiliation": "DWMA", "description": "A demon weapon who transforms into a scythe and is Maka Albarn’s partner."},
+  {"id": 3, "name": "Black Star", "role": "Meister", "affiliation": "DWMA", "description": "A loud and confident meister from the Star Clan who wants to surpass everyone."},
+  {"id": 4, "name": "Tsubaki Nakatsukasa", "role": "Demon Weapon", "affiliation": "DWMA", "description": "A calm demon weapon who partners with Black Star and can transform into multiple ninja weapons."},
+  {"id": 5, "name": "Death the Kid", "role": "Meister", "affiliation": "DWMA", "description": "The son of Death and a meister who is obsessed with symmetry."},
+  ]
--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI 
+from app.data import characters
 
 app = FastAPI()
 
@@ -12,13 +13,6 @@ def get_characters(role: str |  None = None, affiliation: str | None = None):
   filtered_affiliation_characters = []
   filtered_role_affiliation_characters = []
 
-  characters = [
-      {"id": 1, "name": "Maka Albarn", "role": "Meister", "affiliation": "DWMA", "description": "A dedicated meister who partners with Soul Evans and works hard to become stronger."}, 
-      {"id": 2, "name": "Soul Evans", "role": "Demon Weapon", "affiliation": "DWMA", "description": "A demon weapon who transforms into a scythe and is Maka Albarn’s partner."},
-      {"id": 3, "name": "Black Star", "role": "Meister", "affiliation": "DWMA", "description": "A loud and confident meister from the Star Clan who wants to surpass everyone."},
-      {"id": 4, "name": "Tsubaki Nakatsukasa", "role": "Demon Weapon", "affiliation": "DWMA", "description": "A calm demon weapon who partners with Black Star and can transform into multiple ninja weapons."},
-      {"id": 5, "name": "Death the Kid", "role": "Meister", "affiliation": "DWMA", "description": "The son of Death and a meister who is obsessed with symmetry."},
-      ]
 
   if role is not None and affiliation is not None: 
     for character in characters: 
