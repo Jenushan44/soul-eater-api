ef6e5850757d8f500076cd4e0678ffd8a0171e92
Jenushan <jenushan349@gmail.com>
2026-07-03 17:46:47 -0400

    feat: redesign homepage hero section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,8 +7,13 @@ export default function Home() {
     <div>
       <Navbar />
       <div className="w-full relative">
-        <Image src="/Soul-Eater-Banner.png" alt="Banner" width={1200} height={400} className="w-full h-auto relative" priority></Image>
-        <p className="absolute top-[15%] left-[10%]">Welcome to the Soul Eater API</p>
+        <Image src="/Soul-Eater-Medusa-Banner.png" alt="Banner" width={1200} height={400} className="w-full h-auto relative" priority></Image>
+        <p className="absolute top-[16%] left-[2%] font-semibold text-[#b9b761]">WELCOME TO THE</p>
+        <p className="absolute top-[16%] left-[1.5%] font-bold text-[100px]">SOUL EATER</p>
+        <p className="absolute top-[26%] left-[1.5%] font-bold text-[100px]">DATABASE</p>
+        <p className="absolute top-[40%] left-[2%] font-semibold">Explore the world of Soul Eater. Characters, Weapons, Abilities, Organizations and Arcs.</p>
+        <button className="absolute top-[44%] left-[2%] font-semibold cursor-pointer">Explore Database</button>
+        <button className="absolute top-[44%] left-[14%] font-semibold cursor-pointer">LEARN MORE</button>
       </div>
 
       <div className="h-[1000px]" id="character-section">
