a77f48bf6e073f4b23ffef6ce81a5859993d5ae8
Jenushan <jenushan349@gmail.com>
2026-07-04 09:23:15 -0400

    style: improve hero banner responsiveness

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -9,11 +9,11 @@ export default function Home() {
       <div className="w-full relative">
         <Image src="/Soul-Eater-Medusa-Banner.png" alt="Banner" width={1200} height={400} className="w-full h-auto relative" priority></Image>
         <div className="absolute top-[50%] left-[2%]">
-          <p className="font-semibold text-[#f89c0a]">WELCOME TO THE</p>
-          <p className="font-banner text-[100px] leading-[0.8] mt-3">SOUL EATER</p>
-          <p className="font-banner text-[100px] leading-[0.8] text-[#f89c0a] mt-2 mb-2">DATABASE</p>
-          <p className="font-semibold mb-5">Explore the world of Soul Eater. Characters, Weapons, Abilities, Organizations and Arcs.</p>
-          <div className="flex gap-8">
+          <p className="font-semibold text-[#f89c0a] text-[15px] md:text-[20px] lg:text-[30px]">WELCOME TO THE</p>
+          <p className="font-banner text-[60px] md:text-[80px] lg:text-[150px] leading-[0.8] mt-3">SOUL EATER</p>
+          <p className="font-banner text-[60px] md:text-[80px] lg:text-[150px] leading-[0.8] text-[#f89c0a] mt-2 mb-2">DATABASE</p>
+          <p className="font-semibold mb-5 max-w-[500px] text-[15px] md:text-[18px] lg:text-[20px]">Explore the world of Soul Eater. Characters, Weapons, Abilities, Organizations and Arcs.</p>
+          <div className="flex gap-3 md:gap-8">
             <button className="font-semibold cursor-pointer text-black p-3 border-2 border-black rounded-sm bg-[#f89c0a]">EXPLORE DATABASE</button>
             <button className="font-semibold cursor-pointer text-[#f89c0a] p-3 border border-[#f89c0a] rounded-sm">LEARN MORE</button>
           </div>
