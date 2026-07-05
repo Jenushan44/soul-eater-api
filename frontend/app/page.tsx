8b5bf11f25b91020b1357bd7835b38838eb49b70
Jenushan <jenushan349@gmail.com>
2026-07-05 13:07:57 -0400

    feat: add homepage statistics section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar"
+import { Skull, Swords, Flame, MoonStar, BrickWallShield } from 'lucide-react';
 
 export default function Home() {
   return (
@@ -8,16 +9,67 @@ export default function Home() {
       <Navbar />
       <div className="w-full relative">
         <Image src="/Soul-Eater-Medusa-Banner.png" alt="Banner" width={1200} height={400} className="w-full h-auto relative" priority></Image>
-        <div className="absolute top-[50%] left-[2%]">
-          <p className="font-semibold text-[#f89c0a] text-[15px] md:text-[20px] lg:text-[30px]">WELCOME TO THE</p>
-          <p className="font-banner text-[60px] md:text-[80px] lg:text-[150px] leading-[0.8] mt-3">SOUL EATER</p>
-          <p className="font-banner text-[60px] md:text-[80px] lg:text-[150px] leading-[0.8] text-[#f89c0a] mt-2 mb-2">DATABASE</p>
-          <p className="font-semibold mb-5 max-w-[500px] text-[15px] md:text-[18px] lg:text-[20px]">Explore the world of Soul Eater. Characters, Weapons, Abilities, Organizations and Arcs.</p>
+        <div className="absolute 2xl:top-[45%] xl:top-[38%] sm:top-[40%] md:top-[35%] left-[2%]">
+          <p className="font-semibold text-[#f89c0a] text-[15px] md:text-[20px] lg:text-[30px] 2xl:text-[40px]">WELCOME TO THE</p>
+          <p className="font-banner text-[60px] md:text-[80px] lg:text-[150px] 2xl:text-[200px] leading-[0.8] mt-3">SOUL EATER</p>
+          <p className="font-banner text-[60px] md:text-[80px] lg:text-[150px] 2xl:text-[200px] leading-[0.8] text-[#f89c0a] mt-2 mb-2">DATABASE</p>
+          <p className="font-semibold mb-5 max-w-[500px] text-[15px] md:text-[18px] lg:text-[20px] 2xl:text-[23px]">Explore the world of Soul Eater. Characters, Weapons, Abilities, Organizations and Arcs.</p>
           <div className="flex gap-3 md:gap-8">
-            <button className="font-semibold cursor-pointer text-black p-3 border-2 border-black rounded-sm bg-[#f89c0a]">EXPLORE DATABASE</button>
-            <button className="font-semibold cursor-pointer text-[#f89c0a] p-3 border border-[#f89c0a] rounded-sm">LEARN MORE</button>
+            <button className="font-semibold cursor-pointer text-black p-3 2xl:p-8 2xl:text-[20px] border-2 border-black rounded-sm bg-[#f89c0a] hover:bg-black hover:border-[#f89c0a] hover:text-[#f89c0a]">EXPLORE DATABASE</button>
+            <button className="font-semibold cursor-pointer text-[#f89c0a] p-3 2xl:p-8 2xl:text-[20px] border border-[#f89c0a] rounded-sm hover:bg-[#f89c0a] hover:border-black hover:text-black">LEARN MORE</button>
           </div>
         </div>
+
+      </div>
+
+      <div className="h-40 xl:h-50 border rounded-lg border-zinc-800 border-3 flex bg-black gap-2 md:gap-8 lg:gap-15 mx-5">
+        <div className="flex m-auto">
+          <Skull className="w-15 h-15 lg:w-20 lg:h-20 xl:w-25 xl:h-25" fill="#f89c0a" stroke="#000000" strokeWidth={1.5} />
+          <div>
+            <p className="text-[#f89c0a] text-[30px] xl:text-[40px] font-bold">100+</p>
+            <p className="font-semibold text-[14px] lg:text-[18px]">CHARACTERS</p>
+          </div>
+        </div>
+
+        <div className="w-[2px] h-[60%] bg-zinc-800 my-auto" />
+
+        <div className="flex m-auto">
+          <Swords className="w-15 h-15 lg:w-20 lg:h-20 xl:w-25 xl:h-25" fill="#f89c0a" stroke="#000000" strokeWidth={1.5} />
+          <div>
+            <p className="text-[#f89c0a] text-[30px] xl:text-[40px] font-bold">70+</p>
+            <p className="font-semibold text-[14px] lg:text-[18px]">WEAPONS</p>
+          </div>
+        </div>
+
+        <div className="w-[2px] h-[60%] bg-zinc-800 my-auto" />
+        <div className="flex m-auto">
+          <Flame className="w-15 h-15 lg:w-20 lg:h-20 xl:w-25 xl:h-25" fill="#f89c0a" stroke="#000000" strokeWidth={1.5} />
+          <div>
+            <p className="text-[#f89c0a] text-[30px] xl:text-[40px] font-bold">50+</p>
+            <p className="font-semibold text-[14px] lg:text-[18px]">ABILITIES</p>
+          </div>
+        </div>
+
+        <div className="w-[2px] h-[60%] bg-zinc-800 my-auto" />
+
+        <div className="flex m-auto">
+          <BrickWallShield className="w-15 h-15 lg:w-20 lg:h-20 xl:w-25 xl:h-25" fill="#f89c0a" stroke="#000000" strokeWidth={1.5} />
+          <div>
+            <p className="text-[#f89c0a] text-[30px] xl:text-[40px] font-bold">20+</p>
+            <p className="font-semibold text-[14px] lg:text-[18px]">ORGANIZATIONS</p>
+          </div>
+        </div>
+
+        <div className="w-[2px] h-[60%] bg-zinc-800 my-auto" />
+
+        <div className="flex m-auto">
+          <MoonStar className="w-15 h-15 lg:w-20 lg:h-20 xl:w-25 xl:h-25" fill="#f89c0a" stroke="#000000" strokeWidth={1.5} />
+          <div>
+            <p className="text-[#f89c0a] text-[30px] xl:text-[40px] font-bold">30+</p>
+            <p className="font-semibold text-[14px] lg:text-[18px]">ARCS</p>
+          </div>
+        </div>
+
 
       </div>
 
