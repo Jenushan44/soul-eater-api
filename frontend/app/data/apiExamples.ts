3fcc9fd95075d0b565608b5f9cefeb53fb057c2c
Jenushan <jenushan349@gmail.com>
2026-07-09 15:12:00 -0400

    refactor move API examples into data file

---
+++ frontend/app/data/apiExamples.ts
@@ -0,0 +1,40 @@
+const characterExamples = [
+  {
+    id: 1,
+    name: "Maka Albarn",
+    role: "Meister",
+    species: "Human",
+    affiliation: "DWMA",
+    description: "A dedicated, top-model student at the DWMA who ...",
+    image_url: "/characters/characters-maka-albarn.png",
+  },
+
+  {
+    id: 2,
+    name: "Soul Eater Evans",
+    role: "Demon Weapon",
+    species: "Demon Weapon",
+    affiliation: "DWMA",
+    description: "Maka Albarn's weapon partner, who transforms into ...",
+    image_url: "/characters/characters-soul-eater-evans.png",
+  },
+  {
+    id: 3,
+    name: "Death the Kid",
+    role: "Meister",
+    species: "Grim Reaper (Shinigami)",
+    affiliation: "DWMA",
+    description: "The son of Death himself and a powerful Meister who ...",
+    image_url: "/characters/characters-death-the-kid.png",
+  },
+
+  {
+    id: 10,
+    name: "Crona",
+    role: "Meister",
+    species: "Human",
+    affiliation: "None (formerly Medusa's Faction, temporarily DWMA)",
+    description: "The child of the witch Medusa, who raised them through ...",
+    image_url: "/characters/characters-crona.png"
+  }
+];
