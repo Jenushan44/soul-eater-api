5ae1531489b92c754240c22882f58043f6778947
Jenushan <jenushan349@gmail.com>
2026-07-15 18:38:11 -0400

    feat: add ability browse section navigation card

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -178,7 +178,7 @@ export default function Home() {
               <div className="absolute top-0 left-0 w-full h-50 lg:h-85 bg-gradient-to-b from-transparent to-zinc-950" />
 
               <div className="absolute inset-0 gap-3">
-                <Swords className="w-18 h-18 text-[#f89c0a] group-hover:text-[#f89c0a] transition-colors absolute top-25 left-2" />
+                <Swords className="w-18 h-18 text-[#f89c0a] group-hover:text-[#f89c0a] transition-colors absolute top-25 left-3" />
                 <p className='text-[40px] absolute top-43 left-5 font-semibold font-banner'>Weapons</p>
                 <div className="w-[20%] ml-5 h-1 bg-[#f89c0a] mx-auto absolute top-56" />
                 <div className="absolute top-60 left-5">
@@ -196,15 +196,23 @@ export default function Home() {
             </div>
 
             <div className='border border-[#f89c0a66] relative'>
-              <Image className='brightness-[0.3] w-full h-48 lg:h-80 object-cover' src={"/browse-section/abilities-card.png"} alt='Ability Browse Section Image' width={220} height={220}></Image>
-              <div className="absolute top-0 left-0 w-full h-48 lg:h-80 bg-gradient-to-b from-transparent to-zinc-950" />
-
+              <Image className='brightness-[0.3] w-full h-50 lg:h-85 object-cover' src={"/browse-section/abilities-card.png"} alt='Ability Browse Section Image' width={220} height={220}></Image>
+              <div className="absolute top-0 left-0 w-full h-50 lg:h-85 bg-gradient-to-b from-transparent to-zinc-950" />
               <div className="absolute inset-0 gap-3">
-                <Flame className="w-8 h-8 text-zinc-500 group-hover:text-[#f89c0a] transition-colors" />
-                <span className='text-[17px]'>Abilities</span>
+                <Flame className="w-18 h-18 text-[#f89c0a] group-hover:text-[#f89c0a] transition-colors absolute top-25 left-2" />
+                <p className='text-[40px] absolute top-43 left-5 font-semibold font-banner'>ABILITIES</p>
+                <div className="w-[20%] ml-5 h-1 bg-[#f89c0a] mx-auto absolute top-56" />
+                <div className="absolute top-60 left-5 flex flex-col gap-2">
+                  <p className='text-zinc-400'>View soul wavelengths, resonances and special techniques.</p>
+                </div>
+                <div className='flex gap-1 items-center absolute top-75 left-5'>
+                  <p><Flame className='text-[#f89c0a]' /></p>
+                  <p className='font-bold'>50+</p>
+                  <p>Records</p>
+                </div>
               </div>
-              <a href="#ability-section" className="flex items-center justify-between text-zinc-400 hover:text-white hover:bg-red-950/30 px-3 py-2 rounded text-sm font-medium transition-all group border border-transparent hover:border-red-900/30">
-                View Abilities <MoveRight className="-translate-y-1" />
+              <a href="#ability-section" className="text-[#f89c0a] border-zinc-800 flex items-center justify-between font-semibold hover:text-white hover:bg-red-950/30 px-3 py-2 rounded text-sm font-medium transition-all group border border-transparent hover:border-red-900/30">
+                View Abilities <MoveRight className="-translate-y-0" />
               </a>
             </div>
 
