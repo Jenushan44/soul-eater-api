5ae4cbd07ecf8bfb7e42c02d1c4e20a73e4bd6e1
Jenushan <jenushan349@gmail.com>
2026-07-28 18:07:26 -0400

    feat: redesign navbar and homepage hero section

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -4,35 +4,70 @@ import { BookOpen, TriangleAlert, MoveRight } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <div className='flex items-center justify-between bg-black sm:h-[70px] md:h-[100px] lg:h-[130px] xl:h-[150px]'>
+    <div className='flex items-center justify-between bg-black sm:h-[70px] md:h-[100px] lg:h-[130px] xl:h-[150px] border-zinc-900 border-b-2'>
+
+
       <div className='ml-5 mt-[-15] flex-shrink-0'>
-        <Image src="/Soul-Eater-Logo.png" alt="Logo" width={250} height={250}></Image>
+        <Image src="/Soul-Eater-Logo.png" alt="Logo" width={300} height={300}></Image>
       </div>
-      <div className='flex justify-center gap-15'>
+
+
+      <div className='flex justify-between gap-15'>
+
+
         <div className="flex items-center mr-10 gap-15">
-          <a href="#character-section" className="group flex items-center justify-between py-5">
+          <a href="#home-section" className="group flex items-center justify-between py-5">
             <div className="flex items-center gap-4">
               <button className='hover:text-[#f89c0a] transition duration-200 ease-in-out cursor-pointer'>
-                <p className='font-bold'>API Docs</p>
+                <p className='font-semibold text-lg cursor-pointer transition-all duration-300 hover:-translate-y-[0.2px] hover:scale-110 hover:text-xl hover:underline underline-offset-8'>Home</p>
               </button>
               <div className="h-10 w-[2px] ml-10 bg-zinc-700" />
             </div>
           </a>
+
+
+          <a href="#character-section" className="group flex items-center justify-between py-5">
+            <div className="flex items-center gap-4">
+              <button className='hover:text-[#f89c0a] transition duration-200 ease-in-out cursor-pointer'>
+                <p className='font-semibold text-lg cursor-pointer transition-all duration-300 hover:-translate-y-[0.2] hover:scale-110 hover:text-xl hover:underline underline-offset-8'>API Docs</p>
+              </button>
+              <div className="h-10 w-[2px] ml-10 bg-zinc-700" />
+            </div>
+          </a>
+
+          <a href="#character-section" className="group flex items-center justify-between py-5">
+            <div className="flex items-center gap-4">
+              <button className='hover:text-[#f89c0a] transition duration-200 ease-in-out cursor-pointer'>
+                <p className='font-semibold text-lg cursor-pointer transition-all duration-300 hover:-translate-y-[0.2] hover:scale-110 hover:text-xl hover:underline underline-offset-8'>Endpoints</p>
+              </button>
+              <div className="h-10 w-[2px] ml-10 bg-zinc-700" />
+            </div>
+          </a>
+
 
           <a href="https://github.com/Jenushan44/soul-eater-api" target="_blank" rel="noopener noreferrer" className="group flex items-center justify-between py-5">
             <div className="flex items-center gap-4">
               <button className='hover:text-[#f89c0a] transition duration-200 ease-in-out cursor-pointer'>
-                <p className='font-bold'>About</p>
+                <p className='font-semibold text-lg cursor-pointer transition-all duration-300 hover:-translate-y-[0.2] hover:scale-110 hover:text-xl hover:underline underline-offset-8'>About</p>
               </button>
-              <div className="h-10 w-[2px] ml-10 bg-zinc-700" />
             </div>
           </a>
-
-          <button className="group flex items-center justify-between px-4 py-2 my-0 border-2 border-[#f89c0a] rounded-md gap-2 hover:bg-[#f89c0a] hover:text-white transition duration-200 ease-in-out cursor-pointer">
-            <p className='m-0 font-bold'>Report an Issue</p>
-          </button>
         </div>
       </div>
+
+
+      <div className='flex gap-10'>
+        <button className="group flex items-center justify-between py-2 my-0 rounded-md cursor-pointer">
+          <Image src="/github-icon.png" alt="Github Logo" width={40} height={40} className='cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:scale-110'></Image>
+        </button>
+
+
+        <button className="group flex items-center justify-between px-4 py-2 my-0 border-2 border-[#f89c0a] rounded-md gap-2 hover:bg-[#f89c0a] hover:text-white transition duration-200 ease-in-out cursor-pointer mr-10 cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:scale-110">
+          <p className='m-0 font-bold'>Report an Issue</p>
+        </button>
+      </div>
+
+
     </div>
   )
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import EndpointCard from "./components/EndpointCard"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
-import { FunnelX, ChevronRight, ChevronLeft, BookOpen, School, User, Shield, Skull, Swords, Flame, MoonStar, BrickWallShield, MoveRight, Search } from 'lucide-react';
+import { FunnelX, ChevronRight, ChevronLeft, BookOpen, School, User, Shield, Skull, Sword, Swords, Flame, MoonStar, BrickWallShield, MoveRight, Search } from 'lucide-react';
 import Link from 'next/link';
 import { characterExamples, weaponExamples, abilityExamples, organizationExamples, arcExamples, } from "./data/apiExamples";
 
@@ -489,29 +489,31 @@ export default function Home() {
 
 
   return (
-    <div className="bg-black">
+    <div className="bg-black" id="home-section">
       <Navbar />
-      <div className="w-full relative">
-        <Image src="/Soul-Eater-Medusa-Banner.png" alt="Banner" width={1200} height={400} className="w-full h-auto relative" priority></Image>
-        <div className="absolute 2xl:top-[45%] xl:top-[38%] sm:top-[40%] md:top-[35%] left-[2%]">
+      <div className="w-full relative flex justify-end">
+        <Image src="/Soul-Eater-Medusa-Banner.png" alt="Banner" width={1000} height={300} className="w-[60%] h-auto relative" priority></Image>
+        <div className="absolute inset-y-0 left-0 w-[85%] bg-gradient-to-r from-black via-black/100 to-transparent pointer-events-none" />
+
+        <div className="absolute 2xl:top-[5%] xl:top-[5%] sm:top-[5%] md:top-[5%] left-[2%]">
           <p className="font-semibold text-[#f89c0a] text-[15px] md:text-[20px] lg:text-[30px] 2xl:text-[40px]">WELCOME TO THE</p>
           <p className="font-banner text-[60px] md:text-[80px] lg:text-[150px] 2xl:text-[200px] leading-[0.8] mt-3">SOUL EATER</p>
-          <p className="font-banner text-[60px] md:text-[80px] lg:text-[150px] 2xl:text-[200px] leading-[0.8] text-[#f89c0a] mt-2 mb-2">DATABASE</p>
-          <p className="font-semibold mb-5 max-w-[500px] text-[15px] md:text-[18px] lg:text-[20px] 2xl:text-[23px]">Explore the world of Soul Eater. Characters, Weapons, Abilities, Organizations and Arcs.</p>
+          <p className="font-banner text-[60px] md:text-[80px] lg:text-[150px] 2xl:text-[200px] leading-[0.8] text-[#f89c0a] mt-2 mb-2">API</p>
+          <p className="font-semibold mb-5 max-w-[500px] text-[15px] md:text-[18px] lg:text-[20px] 2xl:text-[23px]">A comprehensive REST API for Soul Eater, providing structured data on characters, weapons, abilities, organizations, and story arcs.</p>
           <div className="flex gap-3 md:gap-8">
             <button className="font-semibold cursor-pointer text-black p-3 2xl:p-8 2xl:text-[20px] border-2 border-black rounded-sm bg-[#f89c0a] hover:bg-black hover:border-[#f89c0a] hover:text-[#f89c0a]">EXPLORE DATABASE</button>
             <button className="font-semibold cursor-pointer text-[#f89c0a] p-3 2xl:p-8 2xl:text-[20px] border border-[#f89c0a] rounded-sm hover:bg-[#f89c0a] hover:border-black hover:text-black">LEARN MORE</button>
           </div>
         </div>
-
       </div>
 
-      <div className="h-40 xl:h-50 border rounded-lg border-zinc-800 border-3 flex bg-black gap-2 md:gap-8 lg:gap-15 mx-5">
+
+      <div className="h-40 xl:h-50 border rounded-lg bg-black/70 border-zinc-800 border-3 flex bg-black gap-2 md:gap-8 lg:gap-15 mx-5">
         <div className="flex m-auto">
-          <Image className="w-30 h-auto lg:w-40 xl:w-50" src="/characters-stats-logo.png" alt="Weapon Logo" width={220} height={220}></Image>
+          <User className="w-15 h-15 lg:w-20 lg:h-20 xl:w-25 xl:h-25" fill="#f89c0a" stroke="#000000" strokeWidth={1.5} />
 
           <div>
-            <p className="text-[#f89c0a] text-[30px] xl:text-[40px] font-bold">100+</p>
+            <p className="text-[#f89c0a] text-[30px] xl:text-[40px] font-bold">60+</p>
             <p className="font-semibold text-[14px] lg:text-[18px]">CHARACTERS</p>
           </div>
         </div>
@@ -519,10 +521,10 @@ export default function Home() {
         <div className="w-[2px] h-[60%] bg-zinc-800 my-auto" />
 
         <div className="flex m-auto">
-          <Image className="w-30 h-auto lg:w-40 xl:w-50" src="/weapons-statistics-logo.png" alt="Weapon Logo" width={220} height={220}></Image>
+          <Swords className="w-15 h-15 lg:w-20 lg:h-20 xl:w-25 xl:h-25" fill="#000000" stroke="#f89c0a" strokeWidth={1.5} />
 
           <div>
-            <p className="text-[#f89c0a] text-[30px] xl:text-[40px] font-bold">70+</p>
+            <p className="text-[#f89c0a] text-[30px] xl:text-[40px] font-bold">20+</p>
             <p className="font-semibold text-[14px] lg:text-[18px]">WEAPONS</p>
           </div>
         </div>
@@ -531,7 +533,7 @@ export default function Home() {
         <div className="flex m-auto">
           <Flame className="w-15 h-15 lg:w-20 lg:h-20 xl:w-25 xl:h-25" fill="#f89c0a" stroke="#000000" strokeWidth={1.5} />
           <div>
-            <p className="text-[#f89c0a] text-[30px] xl:text-[40px] font-bold">50+</p>
+            <p className="text-[#f89c0a] text-[30px] xl:text-[40px] font-bold">190+</p>
             <p className="font-semibold text-[14px] lg:text-[18px]">ABILITIES</p>
           </div>
         </div>
@@ -541,7 +543,7 @@ export default function Home() {
         <div className="flex m-auto">
           <BrickWallShield className="w-15 h-15 lg:w-20 lg:h-20 xl:w-25 xl:h-25" fill="#f89c0a" stroke="#000000" strokeWidth={1.5} />
           <div>
-            <p className="text-[#f89c0a] text-[30px] xl:text-[40px] font-bold">20+</p>
+            <p className="text-[#f89c0a] text-[30px] xl:text-[40px] font-bold">5+</p>
             <p className="font-semibold text-[14px] lg:text-[18px]">ORGANIZATIONS</p>
           </div>
         </div>
@@ -551,13 +553,14 @@ export default function Home() {
         <div className="flex m-auto">
           <MoonStar className="w-15 h-15 lg:w-20 lg:h-20 xl:w-25 xl:h-25" fill="#f89c0a" stroke="#000000" strokeWidth={1.5} />
           <div>
-            <p className="text-[#f89c0a] text-[30px] xl:text-[40px] font-bold">30+</p>
+            <p className="text-[#f89c0a] text-[30px] xl:text-[40px] font-bold">20+</p>
             <p className="font-semibold text-[14px] lg:text-[18px]">ARCS</p>
           </div>
         </div>
 
 
       </div>
+
 
       <div className='flex flex-col gap-16 mx-6 mt-10'>
         <div className="sticky top-0 z-50 bg-black p-3 border border-zinc-900 rounded-md tracking-wide">
@@ -772,7 +775,7 @@ export default function Home() {
                     {filteredCharacters.map((character) => (
                       <div key={character.id} className="relative w-75 shrink-0 bg-zinc-900 border border-zinc-800 rounded-lg overflow-hidden transition-transform duration-200 hover:scale-105 hover:border-[#f89c0a]">
                         <div className="relative w-full h-60 bg-zinc-950">
-                          <Image src={character.image_url || "/characters/characters-placeholder.png"} alt="Character Image" fill className="object-cover object-top" />
+                          <Image src={character.image_url || "/characters/characters-placeholder.png"} alt="Character Image" fill className="object-cover" />
                         </div>
                         <div className='absolute top-0 ml-2 mt-2 px-2 border-[#f89c0a] text-[#f8b40a] text-[19px] border-1 rounded-md bg-[#f89c0a]/10'>
                           <p className='font-banner'>{character.role}</p>
@@ -936,7 +939,7 @@ export default function Home() {
                     {filteredWeapons.map((weapon) => (
                       <div key={weapon.id} className="relative w-75 shrink-0 bg-zinc-900 border border-zinc-800 rounded-lg overflow-hidden transition-transform duration-200 hover:scale-105 hover:border-[#f89c0a]">
                         <div className="relative w-full h-60 bg-zinc-950">
-                          <Image src={weapon.image_url || "/characters/characters-placeholder.png"} alt={`${weapon.name} image`} fill className="object-cover object-top" />
+                          <Image src={weapon.image_url || "/characters/characters-placeholder.png"} alt={`${weapon.name} image`} fill className="object-cover" />
                         </div>
 
                         <div className="absolute top-0 ml-2 mt-2 px-2 border-[#f89c0a] text-[#f8b40a] text-[19px] border-1 rounded-md bg-[#f89c0a]/10">
