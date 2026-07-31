410821aedb3035c80218d0d1a7bca2a39bdc5fc1
Jenushan <jenushan349@gmail.com>
2026-07-31 15:10:46 -0400

    refactor: move filter options into separate file

---
+++ frontend/app/data/filterOptions.ts
@@ -0,0 +1,262 @@
+export const weaponTypes = [
+  "Scythe",
+  "Pistol",
+  "Sword",
+  "Spear",
+  "Hammer",
+  "Guillotine",
+  "Multi-Form",
+];
+
+export const weaponCategories = [
+  "Demon Weapon",
+  "Death Scythe",
+  "Shadow Weapon",
+];
+
+export const weaponAffiliations = [
+  "Death Weapon Meister Academy",
+  "Spartoi",
+  "Arachnophobia",
+  "Independent",
+  "None",
+];
+
+export const weaponStatuses = [
+  "Alive",
+  "Deceased",
+  "Unknown",
+  "Destroyed",
+];
+
+export const abilityUsers = [
+  "Akane Hoshi",
+  "Alexandre",
+  "Alone",
+  "Arachne Gorgon",
+  "Asura",
+  "Azusa Yumi",
+  "Black☆Star",
+  "Blair",
+  "Boris Factory Clown",
+  "Crona",
+  "Death the Kid",
+  "Dengu Dinga",
+  "Eibon",
+  "Eruka Frog",
+  "Excalibur",
+  "Feodor",
+  "Fire",
+  "Flying Dutchman",
+  "Franken Stein",
+  "Free",
+  "Gigant",
+  "Giriko",
+  "Gopher",
+  "Grand Witch",
+  "Gravestone",
+  "Great Old One of Power",
+  "Harvar D. Éclair",
+  "Hellworm",
+  "Hero",
+  "House Hoshi",
+  "Inca Kasugatani",
+  "Jacqueline O'Lantern Dupré",
+  "Jester Clown",
+  "Jinn Galland",
+  "Joe Buttataki",
+  "Justin Law",
+  "Kaguya",
+  "Kim Diehl",
+  "Kimial Diehl",
+  "Kirikou Rung",
+  "Little Ogre",
+  "Liz Thompson",
+  "Lord Death",
+  "Maba",
+  "Maka Albarn",
+  "Marie Mjolnir",
+  "Masamune Nakatsukasa",
+  "Medusa Gorgon",
+  "Meme Tatane",
+  "Mifune",
+  "Mizune Family",
+  "Moonlight",
+  "Mosquito",
+  "Nakatsukasa Clan",
+  "Nals Garner",
+  "Noah (Greed)",
+  "Noah (Wrath)",
+  "Ox Ford",
+  "Patty Thompson",
+  "Ponera",
+  "Ragnarok",
+  "Ryūku (Possessed)",
+  "Shaula Gorgon",
+  "Sid Barrett",
+  "Sky Whale",
+  "Sofia Ressa Valk",
+  "Sofia Ressa Valk II",
+  "Soul Eater Evans",
+  "Spirit Albarn",
+  "Tabatha Butterfly",
+  "Tanuki Witches",
+  "Taruho Firefly",
+  "Tezca Tlipoca",
+  "Thunder",
+  "Tsar Pushka",
+  "Tsubaki Nakatsukasa",
+  "Tsumigi Harudori",
+  "White Rabbit",
+  "Witch Judge",
+  "Zubaidah",
+];
+
+export const abilityTypes = [
+  "Weapon Ability",
+  "Soul Ability",
+  "Soul Resonance",
+  "Magic",
+  "Reaper Power",
+  "Book of Eibon Ability",
+  "Black Blood",
+  "Martial Art",
+  "Sensory Ability",
+  "Support Ability",
+  "Defense Ability",
+  "Sword Technique",
+  "Madness",
+];
+
+export const organizationTypes = [
+  "Educational Institution and International Defense Organization",
+  "Elite DWMA Student Unit",
+  "Witch-Led Military Organization",
+  "Rogue Organization",
+  "Independent Organization",
+  "Witches' Governing Council",
+];
+
+export const organizationLeaders = [
+  "Lord Death",
+  "DWMA Faculty and Staff",
+  "Arachne Gorgon",
+  "Medusa Gorgon",
+  "Noah",
+  "Grand Witch Maba",
+];
+
+export const organizationStatuses = [
+  "Active",
+  "Destroyed",
+  "Dissolved",
+  "Defeated",
+];
+
+export const arcCharacters = [
+  "Maka Albarn",
+  "Soul Eater Evans",
+  "Black Star",
+  "Tsubaki Nakatsukasa",
+  "Death the Kid",
+  "Liz Thompson",
+  "Patty Thompson",
+  "Franken Stein",
+  "Sid Barrett",
+  "Crona",
+  "Ragnarok",
+  "Spirit Albarn",
+  "Medusa Gorgon",
+  "Masamune Nakatsukasa",
+  "Mifune",
+  "Angela Leon",
+  "Eruka Frog",
+  "Free",
+  "Mizune",
+  "Lord Death",
+  "Asura",
+  "Arachne Gorgon",
+  "Mosquito",
+  "Giriko",
+  "Joe Buttataki",
+  "Eibon",
+  "Kimial Diehl",
+  "The Clown",
+  "Justin Law",
+  "Marie Mjolnir",
+  "Noah",
+  "Gopher",
+  "Azusa Yumi",
+  "Black Clown",
+  "Excalibur",
+  "Blair",
+  "Tezca Tlipoca",
+  "Maba",
+];
+
+export const arcContinuities = [
+  "Anime and Manga",
+  "Anime Only",
+  "Manga Only",
+];
+
+export const arcEpisodeRanges = [
+  "Episodes 1-12",
+  "Episodes 13-24",
+  "Episodes 25-37",
+  "Episodes 38-51",
+];
+
+export const arcChapterRanges = [
+  "Chapters 0-25",
+  "Chapters 26-50",
+  "Chapters 51-75",
+  "Chapters 76-100",
+  "Chapters 101-113",
+];
+
+export const mainCharacterRoles = [
+  "Meister",
+  "Demon Weapon",
+  "Death Scythe",
+  "Witch",
+  "Grim Reaper",
+  "Kishin",
+  "Sorcerer",
+  "Werewolf",
+  "Demon",
+]
+
+export const characterRoles = [...mainCharacterRoles, "Other"];
+
+export const mainAffiliations = [
+  "Death Weapon Meister Academy",
+  "Arachnophobia",
+  "Witch Order",
+  "Book of Eibon",
+  "Medusa's Faction",
+  "Eight Reaper Legions",
+];
+
+export const affiliations = [...mainAffiliations, "Other"];
+
+export const mainCharacterSpecies = [
+  "Human",
+  "Demon Weapon",
+  "Witch",
+  "Grim Reaper",
+  "Werewolf",
+  "Sorcerer",
+  "Demon",
+];
+
+export const characterStatuses = [
+  "Active",
+  "Alive",
+  "Deceased",
+  "Inactive",
+  "Unknown",
+  "Fused with Crona",
+];
+
+export const characterSpecies = [...mainCharacterSpecies, "Other"];
