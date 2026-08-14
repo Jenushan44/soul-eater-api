a1ae426bbdf86de779582387a510e97140f27801
Jenushan <jenushan349@gmail.com>
2026-08-14 13:28:44 -0400

    feat: add initial character seed insert

--- backend/seed_database.py
+++ backend/seed_database.py
@@ -9,6 +9,7 @@ db_name = os.getenv("DB_NAME")
 db_user = os.getenv("DB_USER")
 db_password = os.getenv("DB_PASSWORD")
 
+# Reads the PostgreSQL connection settings that re passed into the backend container by Docker compose
 connection = psycopg2.connect(
   host = db_host, 
   port = db_port, 
@@ -17,10 +18,11 @@ connection = psycopg2.connect(
   password = db_password
 )
 
+character = characters[0]
+
 cursor = connection.cursor()
-cursor.execute("SELECT version();")
-result = cursor.fetchone()
-print(result)
+cursor.execute("INSERT INTO characters VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (character['id'], character['name'], character['role'], character['affiliation'], character['description'], character['species'], character['sex'], character['soul_type'], character['status'], character['occupations'], character['partners'], character['abilities'], character['debut'], character['continuity'], character['image_url']))
+
 
 cursor.close()
 connection.close()
